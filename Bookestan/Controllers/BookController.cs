08893bcdef563935c32a88e75a34c5e871aff6b1
Vahid Ghasemi <v.ghasemi@gmail.com>
2019-05-10 17:51:02 +0430

    Add project files.

---
+++ Bookestan/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bookestan.Models;
+
+namespace Bookestan.Controllers
+{
+    public class BookController : Controller
+    {
+        BookestanEntities db = new BookestanEntities();
+        // GET: Book
+        [HttpGet]
+        public JsonResult Index(int? id)
+        {
+            if (id != null)
+            {
+                var result = db.USP_Pager(id, 5);
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            else
+                return Json(null, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
